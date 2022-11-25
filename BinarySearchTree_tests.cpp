63dc845101cb146c4a90d349375972a33466eb42
kairo616 <118947890+kairo616@users.noreply.github.com>
2022-11-25 16:38:23 -0500

    Update BinarySearchTree_tests.cpp

--- BinarySearchTree_tests.cpp
+++ BinarySearchTree_tests.cpp
@@ -2,11 +2,91 @@
 
 #include "BinarySearchTree.h"
 #include "unit_test_framework.h"
+#include <sstream>
+
+using namespace std;
 
 
 TEST(test_stub) {
     // Add your tests here
     ASSERT_TRUE(true);
 }
+
+TEST(bst_public_test) {
+  BinarySearchTree<int> tree;
+
+  tree.insert(5);
+
+  ASSERT_TRUE(tree.size() == 1);
+  ASSERT_TRUE(tree.height() == 1);
+
+  ASSERT_TRUE(tree.find(5) != tree.end());
+
+  tree.insert(7);
+  tree.insert(3);
+
+  ASSERT_TRUE(tree.check_sorting_invariant());
+  ASSERT_TRUE(*tree.max_element() == 7);
+  ASSERT_TRUE(*tree.min_element() == 3);
+  ASSERT_TRUE(*tree.min_greater_than(5) == 7);
+
+  cout << "cout << tree.to_string()" << endl;
+  cout << tree.to_string() << endl << endl;
+
+  cout << "cout << tree" << endl << "(uses iterators)" << endl;
+  cout << tree << endl << endl;
+
+  ostringstream oss_preorder;
+  tree.traverse_preorder(oss_preorder);
+  cout << "preorder" << endl;
+  cout << oss_preorder.str() << endl << endl;
+  ASSERT_TRUE(oss_preorder.str() == "5 3 7 ");
+
+  ostringstream oss_inorder;
+  tree.traverse_inorder(oss_inorder);
+  cout << "inorder" << endl;
+  cout << oss_inorder.str() << endl << endl;
+  ASSERT_TRUE(oss_inorder.str() == "3 5 7 ");
+}
+
+TEST(test_empty) {
+    BinarySearchTree<int> i;
+    ASSERT_TRUE(i.empty())
+}
+
+TEST(test_insert) {
+    BinarySearchTree<int> i;
+    i.insert(2);
+    ASSERT_FALSE(i.empty());
+}
+
+TEST(test_size){
+    BinarySearchTree<int> i;
+    i.insert(1);
+    i.insert(7);
+    i.insert(5);
+    ASSERT_EQUAL(i.size(), 0u);
+}
+
+TEST(test_size_zero) {
+    BinarySearchTree<int> i;
+    ASSERT_EQUAL(i.size(), 0u);
+}
+
+TEST(test_height){
+BinarySearchTree<int> i;
+    i.insert(1);
+    i.insert(7);
+    i.insert(5);
+    ASSERT_EQUAL(i.height(), 3u);
+}
+
+TEST(test_find){
+    BinarySearchTree<int> i;
+    i.insert(7);
+    ASSERT_TRUE(i.find(7) != i.end());
+    ASSERT_TRUE(i.find(77) == i.end());
+}
+
 
 TEST_MAIN()
